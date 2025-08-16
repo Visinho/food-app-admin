33be1966ef1670ef4dc9bc31ee446f58a1a6e846
Visinho <Elvisvisinho@rocketmail.com>
2025-08-16 22:17:40 +0100

    Styling in progress -WIP

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+            <img src={assets.add_icon} alt="" />
+            <p className="add-items">Add Items</p>
+        </div>
+        <div className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p className="add-items">List Items</p>
+        </div>
+        <div className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p className="add-items">Orders</p>
+        </div>
+      </div>
     </div>
   )
 }
